94cc1bcb02b543488ca0895de1e4d1ac47121e8d
Johan Bester <jbester@dataserv.com>
2024-11-22 17:19:05 -0600

    add new script

--- CSV-Generator_Scripts/Save_CSV_w_Quotes.py
+++ CSV-Generator_Scripts/Save_CSV_w_Quotes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the Excel file
-df = pd.read_excel('D:/OneDrive - DataServ/Downloads/IPM_CF_Error_Log.csv')
+df = pd.read_csv('D:/OneDrive - DataServ/Downloads/IPM_Labor_Distribution_Copy.csv')
 
 # Save the DataFrame to CSV with UTF-8 encoding and wrap all values in double quotes
-df.to_csv('D:/OneDrive - DataServ/Downloads/IPM_CF_Error_Log_output.csv', index=False, quoting=1, encoding='utf-8')
+df.to_csv('D:/OneDrive - DataServ/Downloads/IPM_Labor_Distribution_Copy_output.csv', index=False, quoting=1, encoding='utf-8')
